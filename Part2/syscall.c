f59b1770e7c4a20d86d308151c4e8d9f6c141255
andrewpetrav <andrewpetravicius1@gmail.com>
2021-11-04 10:49:51 -0500

    Added syscalls for two functions in syscall.c

--- Part2/syscall.c
+++ Part2/syscall.c
@@ -103,6 +103,9 @@ extern int sys_unlink(void);
 extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
+//Added
+extern int sys_GetSharedPage(void);
+extern int sys_FreeSharedPage(void);
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -126,6 +129,10 @@ static int (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
+
+//Added
+[SYS_GetSharedPage]	sys_GetSharedPage,
+[SYS_FreeSharedPage]	sys_FreeSharedPage,
 };
 
 void
