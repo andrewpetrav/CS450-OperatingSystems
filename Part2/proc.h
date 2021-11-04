df489e1e636ccb08cf6c3cb456a78f738f2a1bdf
andrewpetrav <andrewpetravicius1@gmail.com>
2021-11-04 12:11:50 -0500

    I have accidentally been calling shared mapping, shared memory, so it might be worth going back and fixing all of that

--- Part2/proc.h
+++ Part2/proc.h
@@ -34,6 +34,12 @@ struct context {
 
 enum procstate { UNUSED, EMBRYO, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 
+
+//This is to track shared pages
+struct shared_mapping{
+	int key;
+	int *vadd;
+};
 // Per-process state
 struct proc {
   uint sz;                     // Size of process memory (bytes)
@@ -49,6 +55,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  struct shared_mapping shared_mem[64]; //process shared pages
 };
 
 // Process memory is laid out contiguously, low addresses first:
